10d52ab1a05863c90824ad114088381ff60d37fa
Stéphane Le Pajolec <stephane.lepajolec@lapiscine.com>
2023-04-01 19:31:34 +0200

    maj 01/04/23

--- exo-01/js/script.js
+++
@@ -1,9 +0,0 @@
-console.log("Hello world")
-
-const bouton = document.querySelector("button")
-const paragraphe = document.querySelector(".img-hidden")
-
-bouton.addEventListener("click",()=>{
-    paragraphe.style.display = (paragraphe.style.display == "block" ? "none" : "block")
-})
-
